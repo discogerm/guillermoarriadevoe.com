02c245ca24f3cb765565ca12c2192105b508db8b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-07-26 22:18:28 -0400

    Set up basic index, blog, contact, and about pages

---
+++ portfolio-blog/src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/guillermo-arria-devoe/"
+            target="_blank"
+          >
+            Connect on LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/sapinspys" target="_blank">
+            Follow on Github
+          </a>
+        </li>
+        <li>
+          <a href="mailto:guillermo.arriadevoe@gmail.com">
+            guillermo.arriadevoe@gmail.com
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default ContactPage
--- portfolio-blog/src/pages/index.js
+++ portfolio-blog/src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby' // GATSBY BUILT-IN ROUTING
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>I'm Guillermo, a full-stack engineer transitioning from Florida to Utah.</h2>
+      <p>Does your team need a developer? <Link to="/contact">Let's talk!</Link></p>
+    </div>
+  )
+}
+
+export default IndexPage;
