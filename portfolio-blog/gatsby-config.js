87d2d81955dc02c084620e579eb8f7e117a377cd
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-07-30 16:27:29 -0400

    Configured gatsby-source-filesystem

--- portfolio-blog/gatsby-config.js
+++ portfolio-blog/gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
     author: 'Guillermo Arria-Devoe'
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    { // BELOW ENABLES `file` and `allFile` GRAPHQL QUERIES
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/` // NODE, GIVES US ACCESS TO SRC FOLDER
+      }
+    }
   ]
 }
