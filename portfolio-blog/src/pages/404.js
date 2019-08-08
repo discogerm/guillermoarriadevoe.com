17157ea5a07bd6b3ef4858a24cdb4fdcb4af1763
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-08-07 23:31:08 -0400

    Created 404 page and installed React Helmet

---
+++ portfolio-blog/src/pages/404.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const NotFoundPage = () => {
+  return(
+    <Layout>
+      <h1>Page Not Found</h1>
+      <p>
+        <Link to="/">Return Home</Link>
+      </p>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
