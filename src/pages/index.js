dfd42d78641ab8fc517defcb90c2d867c7162fa0
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-09-16 21:53:59 -0700

    Added content to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,31 @@ import Layout from '../components/layout'
 import Head from '../components/head'
 
 import headshot from "../assets/headshot.jpg"
+import styled from './index.module.scss'
 
 const IndexPage = () => {
   return (
     <Layout>
       <div>
         <Head title="Home" /> 
-        <img src={headshot} alt="headshot" width="40%"/>
-        <h1>Helloo!</h1>
-        <h2>I'm Guillermo, a full-stack engineer transitioning from Florida to Utah.</h2>
-        <p>Does your team need a developer? <Link to="/contact">Let's talk!</Link></p>
+        <div className={styled.topContent}>
+          <img src={headshot} alt="headshot" width="30%"/>
+          <div className={styled.introduction}>
+            <h1>Hello</h1>
+            <h2>I'm a full-stack developer recently transplanted to Oregon.</h2>
+            <p>Available for full-time, part-time, and contract opportunities. <Link to="/contact">Let's connect!</Link></p>
+          </div>
+        </div>
+        <div>
+          <h2>Expertise</h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </div> 
     </Layout>
   )
