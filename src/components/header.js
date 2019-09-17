ed8f9ef070d750d59b3670cbe17a212e92567d46
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-09-16 23:05:12 -0700

    Removed about page

--- src/components/header.js
+++ src/components/header.js
@@ -36,15 +36,6 @@ const Header = () => {
             <Link
               className={headerStyles.navItem}
               activeClassName={headerStyles.activeNavItem}
-              to="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={headerStyles.navItem}
-              activeClassName={headerStyles.activeNavItem}
               to="/blog"
             >
               Blog
