c715039a51f022a0b1b232ad4a1e3e90e64a297b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-09-16 22:30:04 -0700

    Added more content to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,25 +16,42 @@ const IndexPage = () => {
           <img src={headshot} alt="headshot" width="30%"/>
           <div className={styled.introduction}>
             <h1>Hello</h1>
-            <h2>I'm a full-stack developer with a touch of designer recently transplanted to Oregon.</h2>
+            <h2>I'm a full-stack developer, with a touch of designer, learning to turn data into valuable insight</h2> 
+            <h2>Portland, Oregon</h2>
             <p>Currently available for full-time, part-time, or contract opportunities. <Link to="/contact">Let's connect!</Link></p>
           </div>
         </div>
-        <div>
-          <h2>Expertise</h2>
+        <div className={styled.summary}>
+          <h2>EXPERTISE</h2>
           <ul>
             <li>Node.js, Express.js, SQLite, server-side JavaScript</li>
             <li>HTML5, CSS3, JavaScript, single page applications</li>
             <li>React, Redux, state management</li>
-            <li>User experience and design</li>
+            <li>User experience (UX) and user interface (UI) design</li>
             <li>RESTful APIs</li>
-            <li>Team leadership, collaboration, communication</li>
+            <li>Team leadership, transparency, communication</li>
             <li>Python, Pandas, Matplotlib, Scikit-Learn, Seaborn</li>
             <li>Data wrangling, storytelling, predictive modeling</li>
-            <li><Link>Learn more</Link></li>
           </ul>
         </div>
-
+        <div className={styled.summary}>
+          <h2>PRACTICES</h2>
+          <ul>
+            <li>Agile software development</li>
+            <li>Collaborative project management</li>
+            <li>Focused, small, testable components</li>
+            <li>Consistent and semantic code</li>
+          </ul>
+        </div>
+        <div className={styled.summary}>
+          <h2>EXPERIENCE</h2>
+          <ul>
+            <li>Freelance web designer and developer</li>
+            <li>Team lead for Lambda School's full-stack web development cohorts</li>
+            <li>5 years experience leading different teams to success</li>
+            <li><Link>Read more</Link></li>
+          </ul>
+        </div>
       </div> 
     </Layout>
   )
