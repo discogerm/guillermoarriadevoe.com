478e742807487de7e9e6105c865fbf81cfbd9e43
sapinspys <garri009@fiu.edu>
2020-06-26 17:38:41 -0400

    Minor warning resolved

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,18 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+// import { graphql, useStaticQuery } from "gatsby"
 
 import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          author
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     site {
+  //       siteMetadata {
+  //         author
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <footer className={footerStyles.footer}>
