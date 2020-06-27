8b4de6336e3f58e810ade8600e64095b6814044d
sapinspys <garri009@fiu.edu>
2020-06-27 14:49:23 -0400

    Updated footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,56 +1,50 @@
 import React from "react"
-// import { graphql, useStaticQuery } from "gatsby"
 
 import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     site {
-  //       siteMetadata {
-  //         author
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
-    <footer className={footerStyles.footer}>
-      <p>"A man's true delight is to do the things he was made for. He was made to show goodwill to his kind, to rise above the promptings of his senses,to distinguish appearances from realities, and to pursue the study of universal Nature and her works.</p>
-      <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/guillermo-arria-devoe/"
-            target="__blank"
-            rel="noopener"
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/guillearria/"
-            target="__blank"
-            rel="noopener"
-          >
-            Instagram
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/sapinspys/"
-            target="__blank"
-            rel="noopener"
-          >
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="mailto:guillermo.arriadevoe@gmail.com">
-            Email
-          </a>
-        </li>
-      </ul>
+    <footer className={footerStyles.siteFooter}>
+      <div className={footerStyles.siteFooterContent}>
+        <p>
+          "A man's true delight is to do the things he was made for. He was made
+          to show goodwill to his kind, to rise above the promptings of his
+          senses,to distinguish appearances from realities, and to pursue the
+          study of universal Nature and her works.
+        </p>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/guillermo-arria-devoe/"
+              target="__blank"
+              rel="noopener"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/guillearria/"
+              target="__blank"
+              rel="noopener"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sapinspys/"
+              target="__blank"
+              rel="noopener"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="mailto:guillermo.arriadevoe@gmail.com">Email</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
