f1405188623430210736f0ad0d6a23cb8184267c
sapinspys <garri009@fiu.edu>
2020-06-29 02:16:21 -0400

    Optimized about page for regular screens

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,77 +22,79 @@ const AboutPage = () => {
           .
         </IntroText>
         <div className={aboutStyles.summary}>
-          <h2>EDUCATION</h2>
-          <ul>
-            <li>
-              <b>Lambda School</b>
-              <br />
-              2019 - now
-              <br />
-              Data Science Track
-            </li>
-            <li>
-              <b>Lambda School</b>
-              <br />
-              2018 - 2019
-              <br />
-              Full Stack Web Development Track
-            </li>
-            <li>
-              <b>Florida International University</b>
-              <br />
-              2013 - 2017
-              <br />
-              Bachelor of Science - Environmental Engineering
-            </li>
-          </ul>
-        </div>
-        <div className={aboutStyles.summary}>
-          <h2>EXPERIENCE</h2>
-          <ul>
-            <li>
-              <b>Lambda School</b>
-              <br />
-              September 2019 - February 2020
-              <br />
-              Remote Section Lead
-            </li>
-            <li>
-              <b>Lambda School</b>
-              <br />
-              April 2019 - August 2020
-              <br />
-              Remote Team Lead
-            </li>
-            <li>
-              <b>The Education Fund - Miami</b>
-              <br />
-              June 2018 - October 2018
-              <br />
-              Independent Consultant
-            </li>
-            <li>
-              <b>Florida International University</b>
-              <br />
-              April 2017 - October 2017
-              <br />
-              Research Assistant
-            </li>
-            <li>
-              <b>Urban Paradise Guild</b>
-              <br />
-              January 2017 - June 2017
-              <br />
-              Senior Intern
-            </li>
-            <li>
-              <b>Urban Paradise Guild</b>
-              <br />
-              April 2014 - January 2016
-              <br />
-              Volunteer Intern
-            </li>
-          </ul>
+          <div className={aboutStyles.summaryContent}>
+            <h2>EDUCATION</h2>
+            <ul>
+              <li>
+                <b>Lambda School</b>
+                <br />
+                2019 - now
+                <br />
+                Data Science Track
+              </li>
+              <li>
+                <b>Lambda School</b>
+                <br />
+                2018 - 2019
+                <br />
+                Full Stack Web Development Track
+              </li>
+              <li>
+                <b>Florida International University</b>
+                <br />
+                2013 - 2017
+                <br />
+                Bachelor of Science - Environmental Engineering
+              </li>
+            </ul>
+          </div>
+          <div className={aboutStyles.summaryContent}>
+            <h2>EXPERIENCE</h2>
+            <ul>
+              <li>
+                <b>Lambda School</b>
+                <br />
+                September 2019 - February 2020
+                <br />
+                Remote Section Lead
+              </li>
+              <li>
+                <b>Lambda School</b>
+                <br />
+                April 2019 - August 2020
+                <br />
+                Remote Team Lead
+              </li>
+              <li>
+                <b>The Education Fund - Miami</b>
+                <br />
+                June 2018 - October 2018
+                <br />
+                Independent Consultant
+              </li>
+              <li>
+                <b>Florida International University</b>
+                <br />
+                April 2017 - October 2017
+                <br />
+                Research Assistant
+              </li>
+              <li>
+                <b>Urban Paradise Guild</b>
+                <br />
+                January 2017 - June 2017
+                <br />
+                Senior Intern
+              </li>
+              <li>
+                <b>Urban Paradise Guild</b>
+                <br />
+                April 2014 - January 2016
+                <br />
+                Volunteer Intern
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </Layout>
