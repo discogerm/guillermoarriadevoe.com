dfca52150c2d09833dd43648cc22aaf78e0b2bde
sapinspys <garri009@fiu.edu>
2020-07-05 18:44:07 -0400

    Created field guide page file

---
+++ src/pages/fieldGuide.js
@@ -0,0 +1,59 @@
+import React from "react"
+// import { Link } from "gatsby" // GATSBY BUILT-IN ROUTING
+
+import Layout from "../components/layout"
+import Head from "../components/head"
+import IntroText from "../components/introText"
+import ProjectCard from "../components/projectCard"
+
+import indexStyles from "./index.module.scss"
+
+const projects = [
+  {
+    url: "https://github.com/sapinspys/GAD-Portfolio-Website",
+    title: "Portfolio Site",
+    desc:
+      "The current website was built from scratch using JavaScript, SCSS, Gatsby.js, GraphQL, and Contentful.",
+    github: "https://github.com/sapinspys/GAD-Portfolio-Website",
+  },
+  {
+    url: "https://mentorsinternational.org/",
+    title: "Mentors International Training Reminders",
+    desc: "Web application for international microfinance training program.",
+    github: "https://github.com/mentors-international-schedule/client",
+  },
+  {
+    url:
+      "https://github.com/team-soup/Ill-Serve-Soup-Marketing-Page--Guillermo-Arria-Devoe",
+    title: "Everybody Eats! Soup Kitchen",
+    desc: "Marketing page for soup kitchen management software.",
+    github:
+      "https://github.com/team-soup/Ill-Serve-Soup-Marketing-Page--Guillermo-Arria-Devoe",
+  },
+]
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <div>
+        <Head title="Home" />
+        <IntroText>
+          Guillermo Arria-Devoe is a Florida-based software engineer, currently
+          enrolled in Lambda School's Data Science program and looking for work.
+        </IntroText>
+        <div className={indexStyles.cardContainer}>
+          {projects.map(p => (
+            <ProjectCard
+              url={p.url}
+              title={p.title}
+              desc={p.desc}
+              github={p.github}
+            />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
