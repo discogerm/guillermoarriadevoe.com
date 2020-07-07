826c9bbb76b1fe91946fb477fe791faadcc36735
sapinspys <garri009@fiu.edu>
2020-07-06 21:23:29 -0400

    Updated photography page for mobile

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import Head from "../components/head"
 import IntroText from "../components/introText"
 
+import photoStyles from "./photography.module.scss"
+
 const Photography = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -21,7 +23,7 @@ const Photography = () => {
           }
         }
       }
-    }  
+    }
   `)
 
   return (
@@ -32,16 +34,17 @@ const Photography = () => {
           One of my favorite hobbies is identifying and researching interesting
           species I find along trails and travels.
         </IntroText>
-        <div>
-          <ul>
-            {data.allContentfulPhotoEntry.edges.map(edge => (
-              <li>
-                <Link to={`/blog/${edge.node.slug}`}>
-                  <img src={edge.node.cardImage.file.url} alt={edge.node.cardImage.title} />
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className={photoStyles.photoGrid}>
+          {data.allContentfulPhotoEntry.edges.map(edge => (
+            <div className={photoStyles.photo}>
+              <Link to={`/photography/${edge.node.slug}`}>
+                <img
+                  src={edge.node.cardImage.file.url}
+                  alt={edge.node.cardImage.title}
+                />
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </Layout>
