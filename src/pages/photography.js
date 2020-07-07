457b960f0f220699dd1160f31d9be782ce3f3c67
sapinspys <garri009@fiu.edu>
2020-07-06 20:47:36 -0400

    Beginning to pull photography data from contentful

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,18 +1,48 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
 import IntroText from "../components/introText"
 
 const Photography = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulPhotoEntry {
+        edges {
+          node {
+            cardImage {
+              file {
+                url
+              }
+              title
+            }
+            slug
+          }
+        }
+      }
+    }  
+  `)
+
   return (
     <Layout>
       <div>
         <Head title="Home" />
         <IntroText>
           One of my favorite hobbies is identifying and researching interesting
-          species.
+          species I find along trails and travels.
         </IntroText>
+        <div>
+          <ul>
+            {data.allContentfulPhotoEntry.edges.map(edge => (
+              <li>
+                <Link to={`/blog/${edge.node.slug}`}>
+                  <img src={edge.node.cardImage.file.url} alt={edge.node.cardImage.title} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Layout>
   )
