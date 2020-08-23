b1ade116a16bc1d8e5407ddd13596e984dda7c81
sapinspys <garri009@fiu.edu>
2020-08-23 01:49:17 -0400

    Removed unecessary link

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,29 +30,23 @@ const BlogPage = () => {
           <ol className={blogStyles.posts}>
             {data.allContentfulBlogPost.edges.map(edge => (
               <li className={blogStyles.post}>
-                <Link to={`/blog/${edge.node.slug}`}>
-                  <h3 className={blogStyles.postTitle}>{edge.node.title}</h3>
-                  <p className={blogStyles.postDate}>
-                    {edge.node.publishedDate}
-                  </p>
-                  <div className={blogStyles.postTags}>
-                    <span>Test, </span>
-                    <span>Test, </span>
-                    <span>Test, </span>
-                    <span>Test</span>
-                  </div>
-                  <div className={blogStyles.postPreview}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Architecto, reprehenderit voluptatum dignissimos ipsum,
-                    minus voluptas rem ullam, expedita error aperiam officia
-                    distinctio quas incidunt sed!
-                  </div>
-                  <div className={blogStyles.postExpand}>
-                    <Link to={`/blog/${edge.node.slug}`}>
-                      Read more...
-                    </Link>
-                  </div>
-                </Link>
+                <h3 className={blogStyles.postTitle}>{edge.node.title}</h3>
+                <p className={blogStyles.postDate}>{edge.node.publishedDate}</p>
+                <div className={blogStyles.postTags}>
+                  <span>Test, </span>
+                  <span>Test, </span>
+                  <span>Test, </span>
+                  <span>Test</span>
+                </div>
+                <div className={blogStyles.postPreview}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto, reprehenderit voluptatum dignissimos ipsum, minus
+                  voluptas rem ullam, expedita error aperiam officia distinctio
+                  quas incidunt sed!
+                </div>
+                <div className={blogStyles.postExpand}>
+                  <Link to={`/blog/${edge.node.slug}`}>Read more...</Link>
+                </div>
               </li>
             ))}
           </ol>
