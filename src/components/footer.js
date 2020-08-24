28a2deace643b71ce640e7b7cb126a83bc2b6121
sapinspys <garri009@fiu.edu>
2020-08-24 11:39:19 -0400

    Removed social links from footer, replaced with copyright

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,58 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(2020)
+
+  useEffect(() => {
+    const d = new Date()
+    const year = d.getFullYear()
+    setCurrentYear(year)
+  }, [])
+
   return (
     <footer>
-      <div className={footerStyles.footerContent}>
-        <ul className={footerStyles.footerMenu}>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/guillermo-arria-devoe/"
-              target="__blank"
-              rel="noopener"
-            >
-              <img
-                src="https://img.icons8.com/color/32/000000/linkedin.png"
-                alt="linkedin"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/guillearria/"
-              target="__blank"
-              rel="noopener"
-            >
-              <img
-                src="https://img.icons8.com/fluent/32/000000/instagram-new.png"
-                alt="instagram"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/arriadevoe/"
-              target="__blank"
-              rel="noopener"
-            >
-              <img
-                src="https://img.icons8.com/nolan/32/github.png"
-                alt="github"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="mailto:guillermo.arriadevoe@gmail.com">
-              <img
-                src="https://img.icons8.com/ultraviolet/32/000000/email-open.png"
-                alt="email"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <p className={footerStyles.footerContent}>
+        &copy; 2019-{currentYear} by Guillermo Arria-Devoe. All rights reserved.{" "}
+        <a href="mailto:guillermo.arriadevoe@gmail.com">Contact me.</a>
+      </p>
     </footer>
   )
 }
