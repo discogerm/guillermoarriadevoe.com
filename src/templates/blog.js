a58a847501bea54e44466cf62897cc2fb32e307c
sapinspys <garri009@fiu.edu>
2020-08-31 17:52:46 -0400

    Cleaning up blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import React from "react"
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-import Layout from '../components/layout'
-import Head from '../components/head'
+import Layout from "../components/layout"
+import Head from "../components/head"
 
-// CONTENTFUL QUERY
+import blogStyles from "./blog.module.scss"
+
 export const query = graphql`
   query($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
+    contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
@@ -16,28 +17,34 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-const Blog = (props) => {
+const Blog = props => {
   const options = {
     renderNode: {
-      "embedded-asset-block": (node) => {
-        const alt = node.data.target.fields.title['en-US'];
-        const url = node.data.target.fields.file['en-US'].url;
-        return <img src={url} alt={alt}/>
-      }
-    }
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img src={url} alt={alt} />
+      },
+    },
   }
 
   return (
     <Layout>
       <Head title={props.data.contentfulBlogPost.title}></Head>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
-      <p>{props.data.contentfulBlogPost.publishedDate}</p>
-      {/* <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div> */}
-      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+      <h1 className={blogStyles.postTitle}>
+        {props.data.contentfulBlogPost.title}
+      </h1>
+      <p className={blogStyles.postDate}>
+        {props.data.contentfulBlogPost.publishedDate}
+      </p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
 
-export default Blog;
+export default Blog
