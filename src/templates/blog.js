cc9443b6c7a69515d67edcbdedfeeef272a5e6b3
sapinspys <garri009@fiu.edu>
2020-08-31 20:29:19 -0400

    Begun styling blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -60,7 +60,54 @@ const Blog = props => {
             </div>
           </div>
         </div>
-        <div className={blogStyles.socialMenu}></div>
+        <div className={blogStyles.socialMenu}>
+          <ul className={blogStyles.menuList}>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/guillermo-arria-devoe/"
+                target="__blank"
+                rel="noopener"
+              >
+                <img
+                  src="https://img.icons8.com/color/40/000000/linkedin.png"
+                  alt="linkedin"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/guillearria/"
+                target="__blank"
+                rel="noopener"
+              >
+                <img
+                  src="https://img.icons8.com/fluent/40/000000/instagram-new.png"
+                  alt="instagram"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/arriadevoe/"
+                target="__blank"
+                rel="noopener"
+              >
+                <img
+                  src="https://img.icons8.com/nolan/40/github.png"
+                  alt="github"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="mailto:guillermo.arriadevoe@gmail.com">
+                <img
+                  src="https://img.icons8.com/ultraviolet/37/000000/email-open.png"
+                  alt="email"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       {documentToReactComponents(
