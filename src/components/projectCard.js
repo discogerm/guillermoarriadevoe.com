4c0cfe694338cb16611ff922754e8d072bb86b9a
sapinspys <garri009@fiu.edu>
2020-10-22 18:52:05 -0400

    Updated projectCard component to accept new props

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -7,13 +7,21 @@ const ProjectCard = props => {
     <div className={cardStyles.card}>
       {/* <img src={require(`../assets/${props.imgUrl}`)} alt={props.imgAlt} /> */}
       <h2 className={cardStyles.cardTitle}>{props.title}</h2>
-      <p className={cardStyles.cardDes}>{props.desc}</p>
+      <div className={cardStyles.technologyTags}>
+        {props.technologyTags.map(tag => (
+          <span>{tag}</span>
+        ))}
+      </div>
+      <p className={cardStyles.cardDes}>{props.description}</p>
       <div className={cardStyles.cardGithub}>
         <img src="https://img.icons8.com/nolan/24/github.png" alt="github" />
-        <a href={props.github} target="__blank" rel="noopener">
-          GitHub
+        <a href={props.githubUrl} target="__blank" rel="noopener">
+          {props.githubUrlTitle}
         </a>
       </div>
+      <a href={props.projectUrl} target="__blank" rel="noopener">
+        {props.projectUrlTitle}
+      </a>
     </div>
   )
 }
