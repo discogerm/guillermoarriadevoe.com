1871384a49f22070dc8c9c2c72e74072faceb51a
sapinspys <garri009@fiu.edu>
2020-10-22 19:00:21 -0400

    Added prop for project image on projectCard

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,12 +5,17 @@ import cardStyles from "./projectCard.module.scss"
 const ProjectCard = props => {
   return (
     <div className={cardStyles.card}>
-      {/* <img src={require(`../assets/${props.imgUrl}`)} alt={props.imgAlt} /> */}
       <h2 className={cardStyles.cardTitle}>{props.title}</h2>
       <div className={cardStyles.technologyUsed}>
         {props.technologyUsed.map(tag => (
           <span>{tag}</span>
         ))}
+      </div>
+      <div className={cardStyles.postPreviewImageContainer}>
+        <img
+          src={props.previewImage.fluid.src}
+          alt={props.previewImage.title}
+        />
       </div>
       <p className={cardStyles.cardDes}>{props.description}</p>
       <div className={cardStyles.cardGithub}>
