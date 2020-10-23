e8f782ce3c41fd3944ac9dadd4de1cb2630193a6
sapinspys <garri009@fiu.edu>
2020-10-23 16:38:47 -0400

    Created new social share menu component

---
+++ src/components/SocialShareMenu.js
@@ -0,0 +1,60 @@
+import React from "react"
+
+import blogStyles from "./blog.module.scss"
+
+import { IconContext } from "react-icons"
+import {
+  AiFillLinkedin,
+  AiFillFacebook,
+  AiFillTwitterSquare,
+} from "react-icons/ai"
+
+const SocialShareMenu = props => {
+  return (
+    <div className={blogStyles.menuContainer}>
+      <ul className={blogStyles.menuList}>
+        <li>
+          <a
+            href={`https://twitter.com/intent/tweet?url=https%3A%2F%2Fguillermoarriadevoe.com%2Fblog%2F${props.data.contentfulBlogPost.slug}`}
+            target="__blank"
+            rel="noopener"
+          >
+            <IconContext.Provider value={{ color: "blue", size: "2em" }}>
+              <div>
+                <AiFillTwitterSquare />
+              </div>
+            </IconContext.Provider>
+          </a>
+        </li>
+        <li>
+          <a
+            href={`https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fguillermoarriadevoe.com%2Fblog%2F${props.data.contentfulBlogPost.slug}`}
+            target="__blank"
+            rel="noopener"
+          >
+            <IconContext.Provider value={{ color: "blue", size: "2em" }}>
+              <div>
+                <AiFillLinkedin />
+              </div>
+            </IconContext.Provider>
+          </a>
+        </li>
+        <li>
+          <a
+            href={`https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fguillermoarriadevoe.com%2Fblog%2F${props.data.contentfulBlogPost.slug}`}
+            target="__blank"
+            rel="noopener"
+          >
+            <IconContext.Provider value={{ color: "blue", size: "2em" }}>
+              <div>
+                <AiFillFacebook />
+              </div>
+            </IconContext.Provider>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default SocialShareMenu
