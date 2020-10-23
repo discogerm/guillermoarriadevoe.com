99981f1cf18fb793149d96af977cc98f95b9ecc0
sapinspys <garri009@fiu.edu>
2020-10-22 21:04:13 -0400

    Updated project card styling

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,28 +5,27 @@ import cardStyles from "./projectCard.module.scss"
 const ProjectCard = props => {
   return (
     <div className={cardStyles.card}>
-      <h2 className={cardStyles.cardTitle}>{props.title}</h2>
-      <div className={cardStyles.technologyUsed}>
-        {props.technologyUsed.map(tag => (
-          <span>{tag}</span>
-        ))}
-      </div>
       <div className={cardStyles.postPreviewImageContainer}>
         <img
           src={props.previewImage.fluid.src}
           alt={props.previewImage.title}
         />
       </div>
+      <h2 className={cardStyles.cardTitle}>{props.title}</h2>
+      <div className={cardStyles.technologyUsed}>
+        {props.technologyUsed.map(tag => (
+          <span>{tag}</span>
+        ))}
+      </div>
       <p className={cardStyles.cardDes}>{props.description}</p>
-      <div className={cardStyles.cardGithub}>
+      <div className={cardStyles.linksContainer}>
         <a href={props.gitHubUrl} target="__blank" rel="noopener">
           {props.gitHubUrlTitle}
         </a>
+        <a href={props.projectUrl} target="__blank" rel="noopener">
+          {props.projectUrlTitle}
+        </a>
       </div>
-      {props.githubUrlTitle}
-      <a href={props.projectUrl} target="__blank" rel="noopener">
-        {props.projectUrlTitle}
-      </a>
     </div>
   )
 }
