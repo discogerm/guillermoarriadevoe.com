741e2f6ef5e3912eb845dc3ec020eda70c5de6d7
sapinspys <garri009@fiu.edu>
2020-10-22 22:05:47 -0400

    Styled technology tags for mobile

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -5,20 +5,20 @@ import cardStyles from "./projectCard.module.scss"
 const ProjectCard = props => {
   return (
     <div className={cardStyles.card}>
-      <div className={cardStyles.postPreviewImageContainer}>
+      <div className={cardStyles.imageContainer}>
         <img
           src={props.previewImage.fluid.src}
           alt={props.previewImage.title}
         />
       </div>
       <h2 className={cardStyles.cardTitle}>{props.title}</h2>
-      <div className={cardStyles.technologyUsed}>
+      <div className={cardStyles.technologyTags}>
         {props.technologyUsed.map(tag => (
           <span>{tag}</span>
         ))}
       </div>
       <p className={cardStyles.cardDes}>{props.description}</p>
-      <div className={cardStyles.linksContainer}>
+      <div className={cardStyles.linkContainer}>
         <a href={props.gitHubUrl} target="__blank" rel="noopener">
           {props.gitHubUrlTitle}
         </a>
