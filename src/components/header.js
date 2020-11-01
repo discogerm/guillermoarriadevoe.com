46dc5735f101ca3ac3d2d51908711bdc019cf195
sapinspys <garri009@fiu.edu>
2020-11-01 01:10:13 -0500

    Removed warnigns

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,7 @@
-import React, { useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 import headerStyles from "./header.module.scss"
-
-import Media from "react-media"
-import HamburgerMenu from "react-hamburger-menu"
 
 import SocialMenu from "./socialMenu"
 
