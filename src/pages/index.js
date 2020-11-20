928d7d0b8d367982d73ca9f0866661829a35de6b
sapinspys <garri009@fiu.edu>
2020-11-20 17:20:53 -0500

    Created new component for tag filters

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,11 +32,39 @@ const BlogPage = () => {
     }
   `)
 
+  const create_tag_collection = edges => {
+    var tag_collection = []
+
+    edges.map(edge => tag_collection.push(...edge.node.tags))
+
+    let unique = [...new Set(tag_collection)]
+    return unique
+  }
+
   return (
     <Layout>
       <div>
         <Head title="Blog" />
         <div>
+          {/* <div>{create_tag_collection(data.allContentfulBlogPost.edges)}</div> */}
+          {/* <div>
+            {data.allContentfulBlogPost.edges.map(edge => {
+              if (edge.node.tags.includes("Personal")) {
+                return <div>{edge.node.tags}</div>
+              } else {
+                return null
+              }
+            })}
+          </div> */}
+          <div>
+            {data.allContentfulBlogPost.edges.map(edge =>
+              edge.node.tags.includes("Personal") ? (
+                <div>{edge.node.tags}</div>
+              ) : (
+                []
+              )
+            )}
+          </div>
           <ol className={blogStyles.postContainer}>
             {data.allContentfulBlogPost.edges.map(edge => (
               <BlogPostCard
