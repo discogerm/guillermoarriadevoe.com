47d9600eaba09f306a937efb91c5059534d94edb
sapinspys <garri009@fiu.edu>
2020-11-23 23:42:39 -0500

    Restored name, too much of a hassle

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@
 
 module.exports = {
   siteMetadata: {
-    title: "William Arria-Devoe",
-    author: "William Arria-Devoe",
+    title: "Guillermo Arria-Devoe",
+    author: "Guillermo Arria-Devoe",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
