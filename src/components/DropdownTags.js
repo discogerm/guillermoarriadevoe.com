8b53da662b738860d2d218e6ffecb62e9eec2813
sapinspys <garri009@fiu.edu>
2020-12-16 00:38:46 -0500

    Begun creating dropdown filters

---
+++ src/components/DropdownTags.js
@@ -0,0 +1,8 @@
+import React, { useState} from 'react';
+
+function Dropdown({ title, items, multiSelect = false}) {
+    const [open, setOpen] = useState(false);
+    const [selection, setSelection] = useState([])
+}
+
+export default Dropdown;
