72ce610fc963f4cfc511bb032e4678143ef36c22
sapinspys <garri009@fiu.edu>
2020-12-17 02:41:25 -0500

    Updated DD filter component

--- src/components/DropdownFilters.js
+++ src/components/DropdownFilters.js
@@ -1,8 +1,24 @@
-import React, { useState} from 'react';
+import React, { useState } from "react"
 
-function Dropdown({ title, items, multiSelect = false}) {
-    const [open, setOpen] = useState(false);
-    const [selection, setSelection] = useState([])
+function Dropdown({ title, items, multiSelect = false }) {
+  const [open, setOpen] = useState(false)
+  const [selection, setSelection] = useState([])
+
+  return (
+    <div className="dd-wrapper">
+      <div
+        tabIndex={0}
+        className="dd-header"
+        role="button"
+        onKeyPress={}
+        onClick={}
+      >
+          <div className="title">
+              <p>{filter}</p>
+          </div>
+      </div>
+    </div>
+  )
 }
 
-export default Dropdown;
+export default Dropdown
