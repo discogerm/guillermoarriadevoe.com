9bccd81c2ff996d4cb119380db1a9ea73b3bb4f9
sapinspys <garri009@fiu.edu>
2020-12-17 02:43:18 -0500

    Migrated content to DD filter component

--- src/components/DropdownFilters.js
+++ src/components/DropdownFilters.js
@@ -4,6 +4,16 @@ function Dropdown({ title, items, multiSelect = false }) {
   const [open, setOpen] = useState(false)
   const [selection, setSelection] = useState([])
 
+  // How many times is this running?
+  const create_tag_collection = edges => {
+    var tagCollection = []
+
+    edges.map(edge => tagCollection.push(...edge.node.tags))
+
+    let uniqueTags = [...new Set(tagCollection)]
+    return uniqueTags
+  }
+
   return (
     <div className="dd-wrapper">
       <div
@@ -13,9 +23,9 @@ function Dropdown({ title, items, multiSelect = false }) {
         onKeyPress={}
         onClick={}
       >
-          <div className="title">
-              <p>{filter}</p>
-          </div>
+        <div className="title">
+          <p>{filter}</p>
+        </div>
       </div>
     </div>
   )
