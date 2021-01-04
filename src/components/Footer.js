5de3b7000520d13342e28d7dc3a479bda567f711
sapinspys <garri009@fiu.edu>
2021-01-03 23:07:02 -0500

    Updated footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 
 import footerStyles from "./Footer.module.scss"
 
-import SocialMenu from "./SocialMenu"
+// import SocialMenu from "./SocialMenu"
 
 const Footer = () => {
   const [currentYear, setCurrentYear] = useState(2020)
@@ -18,7 +18,7 @@ const Footer = () => {
       <div className={footerStyles.contentContainer}>
         {/* <SocialMenu /> */}
         <p className={footerStyles.copyright}>
-          &copy; 2019-{currentYear} by Guillermo Arria-Devoe.{" "}
+          Made with &hearts; by Guillermo Arria-Devoe.{" "}
           <a href="mailto:guillermo.arriadevoe@gmail.com">Questions?</a>
         </p>
       </div>
